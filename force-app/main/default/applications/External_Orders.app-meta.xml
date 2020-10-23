785e8f9018d746c8bd6b563f85849e0172a8f469
mharikian <mharikian@navexglobal.com>
2020-10-23 09:47:57 -0700

    first commit

---
+++ force-app/main/default/applications/External_Orders.app-meta.xml
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomApplication xmlns="http://soap.sforce.com/2006/04/metadata">
+    <defaultLandingTab>Quickstart</defaultLandingTab>
+    <isNavAutoTempTabsDisabled>false</isNavAutoTempTabsDisabled>
+    <isNavPersonalizationDisabled>false</isNavPersonalizationDisabled>
+    <label>External Orders</label>
+    <tabs>Quickstart</tabs>
+</CustomApplication>
---
+++ force-app/main/default/aura/accEdit/accEditController.js
@@ -0,0 +1,28 @@
+({
+	handleSaveAccount : function(component, event, helper) {
+        if(helper.validateAccountForm(component)) {
+            component.set("v.accountRecord.Name", component.get("v.recordId"));
+            component.find("accountRecordCreator").saveRecord(function(saveResult) {
+                if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                    // record is saved successfully
+                    var resultsToast = $A.get("e.force:showToast");
+                    resultsToast.setParams({
+                        "title": "Saved",
+                        "message": "The record was saved."
+                    });
+                    resultsToast.fire();
+                } else if (saveResult.state === "INCOMPLETE") {
+                    // handle the incomplete state
+                    console.log("User is offline, device doesn't support drafts.");
+                } else if (saveResult.state === "ERROR") {
+                    // handle the error state
+                    console.log('Problem saving contact, error: ' +
+                                 JSON.stringify(saveResult.error));
+                } else {
+                    console.log('Unknown problem, state: ' + saveResult.state +
+                                ', error: ' + JSON.stringify(saveResult.error));
+                }
+            });
+		}
+	}
+})
